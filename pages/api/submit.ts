2b1f02440c3569d92965bb40866273a3cee52c29
Philani Ncube <ncbphi001@gmail.com>
2022-11-10 20:02:54 +0200

    leadership congres first cmmit

---
+++ pages/api/submit.ts
@@ -0,0 +1,76 @@
+import mailchimp from '@mailchimp/mailchimp_marketing'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import serviceRole from '../../utils/serviceRole';
+
+mailchimp.setConfig({
+  apiKey: process.env.API_KEY,
+  server: process.env.SERVER_PREFIX,
+})
+
+const listID = process.env.LIST_ID || ''
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+ const { first_name,
+      last_name,
+      designation,
+      country,
+      diet,
+      email,
+      id_number,
+      organisation,
+      tel,} = req.body
+
+    const { data, error } = await serviceRole.from('maritime_leadership_conference').insert([{ first_name,
+      last_name,
+      designation,
+      country,
+      diet,
+      email,
+      id_number,
+      organisation,
+      tel}])
+
+    console.log({data, error})
+
+
+           const subscribingUser = { first_name,
+      last_name,
+      designation,
+      country,
+      diet,
+      email,
+      id_number,
+      organisation,
+      tel}
+
+
+
+  try {
+    const response = await mailchimp.lists.addListMember(listID, {
+      email_address: subscribingUser.email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: subscribingUser.first_name,
+        LNAME: subscribingUser.last_name,
+        PHONE: subscribingUser.tel,
+        TITLE: subscribingUser.designation,
+        COUNTRY: subscribingUser.country,
+        DIET: subscribingUser.diet,
+        IDNUM: subscribingUser.id_number,
+        ORG: subscribingUser.organisation
+      }
+
+    })
+
+
+
+    res.status(200).json({ response, data })
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ error })
+  }
+
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,257 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true)
+    const {first_name, last_name, designation, country, diet, email, id_number, organisation, tel} = Object.fromEntries(new FormData(e.currentTarget));
+    console.log({
+      first_name,
+      last_name,
+      designation,
+      country,
+      diet,
+      email,
+      id_number,
+      organisation,
+      tel,
+    });
+
+
+    const res = await fetch(`/api/submit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        designation,
+        country,
+        diet,
+        email,
+        id_number,
+        organisation,
+        tel,
+      }),
+    });
+
+
+     if (res.ok) {
+       const result = await res.json();
+       setLoading(false);
+
+       console.log(result);
+
+       alert("Thank You for your submission");
+       setLoading(false);
+     } else {
+       const result = await res.json();
+       console.log(result.response);
+       alert(
+         `There was an error. Please try again later. Please remember, you cannot sign up with the same email address more than once.`
+       );
+       setLoading(false);
+     }
+
+
+    setLoading(false)
+  }
+
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <main className="max-w-5xl mx-auto px-4 py-12">
+        <Image
+          src="/images/banner_2.jpg"
+          alt="Banner"
+          className="w-full mx-auto object-cover"
+          width={1890}
+          height={591}
+        />
+        <form onSubmit={handleSubmit} className="w-full py-3 px-2">
+          <div className="grid grid-cols-6 gap-3">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="first_name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                autoComplete="given-name"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="last_name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                autoComplete="family-name"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <div className="block" aria-hidden="true">
+            <div className="py-8">
+              <div className="border-t border-gray-200" />
+            </div>
+          </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+          <div className="grid grid-cols-6 gap-3">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="email"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="tel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Contact Number
+              </label>
+              <input
+                type="text"
+                name="tel"
+                id="tel"
+                autoComplete="tel"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+          <div className="block" aria-hidden="true">
+            <div className="py-8">
+              <div className="border-t border-gray-200" />
+            </div>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div className="grid grid-cols-6 gap-3">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="designation"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Designation
+              </label>
+              <select
+                id="designation"
+                name="designation"
+                autoComplete="designation"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              >
+                <option>Mr</option>
+                <option>Miss</option>
+                <option>Mrs</option>
+                <option>Professor</option>
+              </select>
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="diet"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Diet
+              </label>
+              <select
+                id="diet"
+                name="diet"
+                autoComplete="diet"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              >
+                <option>Halaal</option>
+                <option>Vegetarian</option>
+                <option>Vegan</option>
+                <option>None</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-9 mt-3 gap-3">
+            <div className="col-span-9 sm:col-span-3">
+              <label
+                htmlFor="organisation"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Organisation/Institute
+              </label>
+              <input
+                type="text"
+                name="organisation"
+                id="organisation"
+                autoComplete="organisation"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="col-span-9 sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Country
+              </label>
+              <input
+                type="text"
+                name="country"
+                id="country"
+                autoComplete="country"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="col-span-9 sm:col-span-3">
+              <label
+                htmlFor="id_number"
+                className="block text-sm font-medium text-gray-700"
+              >
+                ID Number/Passport
+              </label>
+              <input
+                type="text"
+                name="id_number"
+                id="id_number"
+                autoComplete="id_number"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+          <button type="submit" disabled={loading} className="bg-indigo-600 rounded-lg shadow font-medium uppercase text-white px-8 mt-4 py-2 w-fit">{loading ? 'Loading...' : 'Submit'}</button>
+        </form>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+    </>
+  );
 }
 
 export default Home
